149a7c8145abff02e991398016e8742b8ae90d0b
Odiaz Bumma <odiazbumma@gmail.com>
2019-12-27 01:06:44 +0700

    Challenge 1: packages and external libraries, done

---
+++ section-11/Series/src/com/examplepackages/Series.java
@@ -0,0 +1,34 @@
+package com.examplepackages;
+
+public class Series {
+
+    public static long nSum(long n) {
+        if (n == 0)
+            return 0;
+
+        if (n == 1)
+            return 1;
+
+        return n + nSum(n-1);
+    }
+
+    public static long factorial(long n) {
+        if (n == 0)
+            return 0;
+
+        if (n == 1)
+            return 1;
+
+        return n * factorial(n-1);
+    }
+
+    public static long fibonacci(long n) {
+        if (n == 0)
+            return 0;
+
+        if (n == 1)
+            return 1;
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
